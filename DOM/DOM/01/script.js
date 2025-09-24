47f1b2f194d9ac27287f925eec993ed42942716e
abdulhaimaaz <maaz.codes1@gmail.com>
2025-09-24 23:32:21 +0530

    new

--- DOM/DOM/01/script.js
+++
@@ -1,9 +0,0 @@
-function createElementH2(){
-    let h2 = document.createElement('h2');
-    h2.innerText = "I'm H2 Element created by you!";
-
-    let div = document.getElementById('my-container');
-    div.append(h2);
-
-    console.log(h2);
-}
